241360b6e5e2100726a9382c0d59485b19eeb158
maitreyee-github <104962708+maitreyee-github@users.noreply.github.com>
2022-07-12 13:13:37 +0530

    Update __init__.py

--- wger/core/models/__init__.py
+++ wger/core/models/__init__.py
@@ -14,7 +14,7 @@
 #  You should have received a copy of the GNU Affero General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Local
+# import local libraries
 from .cache import UserCache
 from .days_of_week import DaysOfWeek
 from .language import Language
